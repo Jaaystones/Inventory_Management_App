40a31b05e77bf5bb0f7d45c60f5504a8eac7dcee
Jaaystones <Joelpaul345@gmail.com>
2023-09-13 16:21:27 +0100

    Adjusted files and routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import registerUser from '../controllers/registerUser.js';
+import { loginUser, logOut }  from '../controllers/logInLogOutUser.js';
+import getUser from '../controllers/getUsers.js';
+import Secure from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
 
 router.post('/register', registerUser);
-
+router.post('/login', loginUser);
+router.get('/logout', logOut);
+router.get('/getuser', Secure, getUser);
 
 export default router;
