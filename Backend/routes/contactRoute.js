0fe7edeb94d07cec85fde3daf113f8974f7fcdc9
Jay stones <Joelpaul345@gmail.com>
2023-09-28 10:07:12 +0100

    :white_check_mark: Contact us route

---
+++ Backend/routes/contactRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { contactUs } from "../controllers/contactController.js";
+import Secure from "../middleWare/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", Secure, contactUs);
+
+export default router;
